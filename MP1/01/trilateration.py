2a7b52c6d399175efdecac6e7e1af8fdb28403f0
yang-sam <sam.yang95@rutgers.edu>
2017-10-17 20:50:40 -0400

    trilateration working???

--- MP1/01/trilateration.py
+++ MP1/01/trilateration.py
@@ -1,11 +1,46 @@
 import sys
-
+import numpy as np
+from math import *
 # Takes in a list of lists, each list is [x,y,z,d]
 def trilaterate3D(distances):
-    #navigate through list
+    vect1 = np.array(distances[0])
+    vect2 = np.array(distances[1])
+    vect3 = np.array(distances[2])
+    vect4 = np.array(distances[3])
+
+    # #creating the translation vector to set the first vector at the origin
+    # ox = vect1[0]*-1
+    # oy = vect1[1]*-1
+    # oz = vect1[2]*-2
+    # translate = np.array([ox, oy, oz, 0])
+
+    # #translating all vectors in relation, treating the first vector as origin
+    # vect1 = vect1 + translate
+    # vect2 = vect2 + translate
+    # vect3 = vect3 + translate
+    # vect4 = vect4 + translate
+    dist = [vect1[3], vect2[3], vect3[3]]
+
+    vect1 = vect1[0:3]
+    vect2 = vect2[0:3]
+    vect3 = vect3[0:3]
+
+    #transform circle 1 to 0
+    ox = (vect2-vect1)/np.linalg.norm(vect2-vect1)
+    i = np.dot(ox, vect3-vect1)
+    oy = (vect3-vect1-i*ox)/(np.linalg.norm(vect3-vect1-i*ox))
+    oz = np.cross(ox,oy)
+    d = np.linalg.norm(vect2 - vect1)
+    j = np.dot(oy, vect3 - vect1)
     #compute trilaterization
+
+    x = (pow(dist[0],2) - pow(dist[1],2) + pow(d,2))/(2*d)
+    y = ((pow(dist[0],2) - pow(dist[2],2) + pow(i,2) + pow(j,2))/(2*j)) - ((i/j)*x)
+    z = sqrt(pow(dist[0],2) - pow(x,2) - pow(y,2))
+
+    print x,y,z
     #returns local point
-    return [0.,0.,0.,0.]
+    return [x,y,z]
 
 if __name__ == "__main__":
     
